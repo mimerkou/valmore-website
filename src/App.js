aab2b1b24f5f0046acd0fc410079fcb5986c8a8d
Michalis Merkouris <mimerkou@gmail.com>
2021-10-26 19:26:46 +0300

    installed aos package for animations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import AOS from 'aos';
+
+AOS.init();
 
 const App = () => {
   return (
