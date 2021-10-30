d3715d9e4688ea6565fa556f580fe9424218e6b2
Michalis Merkouris <mimerkou@gmail.com>
2021-10-30 21:23:54 +0300

    started working on services section

---
+++ src/components/Services/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ServicesContainer } from './ServicesElements';
+import 'aos/dist/aos.css';
+
+const Services = () => {
+  return (
+    <>
+      <ServicesContainer id="services"></ServicesContainer>
+    </>
+  );
+};
+
+export default Services;
---
+++ src/components/Services/ServicesElements.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const ServicesContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+  min-height: 100vh;
+  max-width: 110rem;
+  margin: 0 auto;
+  padding: 10rem 0;
+`;
+
+// export const TextWrapper = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-direction: column;
+// `;
