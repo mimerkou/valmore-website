58df6ce2375810c0d3aefa9188ea3dca75b94d4d
Michalis Merkouris <mimerkou@gmail.com>
2021-10-31 21:39:40 +0200

    started updating Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,10 +3,12 @@ import { Link } from 'react-scroll';
 
 export const Button = styled(Link)`
   border-radius: 50px;
-  background: ${({ primary }) => (primary ? '#f48599' : '#010606')};
+  /* background: ${({ primary }) => (primary ? '#f48599' : '#010606')}; */
+  background: #f48599;
   white-space: nowrap;
   padding: 1.2rem 3rem;
-  color: ${({ dark }) => (dark ? '#010606' : '#fff')};
+  /* color: ${({ dark }) => (dark ? '#010606' : '#fff')}; */
+  color: #010606;
   font-size: 1.6rem;
   outline: none;
   border: none;
@@ -14,10 +16,11 @@ export const Button = styled(Link)`
   display: flex;
   justify-content: center;
   align-items: center;
-  transition: all 0.2s ease-in-out;
+  transition: all 0.3s ease-in-out;
 
   &:hover {
-    transition: all 0.2s ease-in-out;
-    background: ${({ primary }) => (primary ? '#fff' : '#f48599')};
+    transition: all 0.3s ease-in-out;
+    /* background: ${({ primary }) => (primary ? '#fff' : '#f48599')}; */
+    background: #fff;
   }
 `;
