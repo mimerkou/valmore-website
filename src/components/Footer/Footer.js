be0a683854cc6dad98a2ce26c621009199e00ae2
Michalis Merkouris <mimerkou@gmail.com>
2021-11-02 12:25:03 +0200

    finished footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import {
+  FooterContainer,
+  FooterWrapper,
+  Social,
+  SocialText,
+  SocialIcons,
+  Facebook,
+  Linkedin,
+  Carrers,
+  CarrerLink,
+} from './FooterElements';
 
 const Footer = () => {
-  return <></>;
+  return (
+    <>
+      <FooterContainer>
+        <FooterWrapper>
+          <Social>
+            <SocialText>You can find us at:</SocialText>
+            <SocialIcons>
+              <Facebook>
+                <a
+                  href="https://www.facebook.com/valmoretechnologies"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className="fab fa-facebook-square"></i>
+                </a>
+              </Facebook>
+              <Linkedin>
+                <a
+                  href="https://www.linkedin.com/company/valmore-technologies/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className="fab fa-linkedin"></i>
+                </a>
+              </Linkedin>
+            </SocialIcons>
+          </Social>
+
+          <Carrers>
+            <CarrerLink to="/careers">Carrers</CarrerLink>
+          </Carrers>
+        </FooterWrapper>
+      </FooterContainer>
+    </>
+  );
 };
 
 export default Footer;
--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -1,2 +1,65 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+
+export const FooterContainer = styled.div`
+  min-height: 20vh;
+  background-color: #505050;
+  color: #fff;
+`;
+
+export const FooterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  max-width: 110rem;
+  margin: 0 auto;
+`;
+
+export const Social = styled.div`
+  margin-top: 3rem;
+`;
+
+export const SocialText = styled.div`
+  font-size: 1.8rem;
+`;
+
+export const SocialIcons = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & i {
+    color: #fff;
+    transition: all 0.3s;
+  }
+
+  & i:hover {
+    color: #f48599;
+  }
+`;
+
+export const Facebook = styled.div`
+  margin-right: 3rem;
+  font-size: 5rem;
+`;
+
+export const Linkedin = styled.div`
+  font-size: 5rem;
+`;
+
+export const Carrers = styled.div`
+  margin: 2.5rem 0;
+`;
+
+export const CarrerLink = styled(Link)`
+  font-size: 1.8rem;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: #fff;
+  transition: all 0.3s;
+
+  &:hover {
+    color: #f48599;
+  }
+`;
