7d899290882b073d85056e6d68c125e7735e019f
Michalis Merkouris <mimerkou@gmail.com>
2021-11-04 19:11:53 +0200

    finished NotFound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -11,8 +11,50 @@ const ErrorPage = styled.div`
   background: #13152a;
 `;
 
+const ErrorInfo = styled.div`
+  margin: 2rem;
+`;
+
+const ErrorH2 = styled.h2`
+  border: 1px solid #fff;
+  font-size: 4.8rem;
+  padding: 1rem 2rem;
+  color: #13152a;
+  background-color: #fff;
+`;
+
+const ErrorP = styled.p`
+  margin-top: 2rem;
+  margin-bottom: 5rem;
+  color: #fff;
+  font-size: 1.8rem;
+`;
+
+const HomeBtn = styled(Link)`
+  text-decoration: none;
+  white-space: nowrap;
+  border-radius: 5rem;
+  padding: 1.2rem 3rem;
+  background: #f48599;
+  color: #010606;
+  transition: all 0.3s;
+
+  &:hover {
+    background: #fff;
+    color: #010606;
+  }
+`;
+
 const NotFound = () => {
-  return <ErrorPage></ErrorPage>;
+  return (
+    <ErrorPage>
+      <ErrorInfo>
+        <ErrorH2>Oops! Page not found</ErrorH2>
+        <ErrorP>The page you were looking for does not exist.</ErrorP>
+        <HomeBtn to="/">Back Home</HomeBtn>
+      </ErrorInfo>
+    </ErrorPage>
+  );
 };
 
 export default NotFound;
