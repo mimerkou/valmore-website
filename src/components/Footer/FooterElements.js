0bd832d55806834fd7cd1274889e3585316ac9a9
Michalis Merkouris <mimerkou@gmail.com>
2021-11-05 04:31:56 +0200

    tried different styles for the Career Link in footer section

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -65,10 +65,27 @@ export const CarrerLink = styled(Link)`
   border: 1px solid #fff;
   border-radius: 5px;
   padding: 1rem 2rem;
+  /* animation: animateLink 3s infinite; */
   transition: all 0.3s;
+  /* box-shadow: 0.4px 0.4px 3px #f48599; */
 
   &:hover {
     color: #f48599;
     border-color: #f48599;
   }
+
+  /* @keyframes animateLink {
+    0% {
+      color: #fff;
+      border-color: #fff;
+    }
+    50% {
+      color: #f48599;
+      border-color: #f48599;
+    }
+    100% {
+      color: #fff;
+      border-color: #fff;
+    }
+  } */
 `;
